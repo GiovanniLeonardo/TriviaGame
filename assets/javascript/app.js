51f9f2eed00d5a5979f979be4799de04aa3391a0
GiovanniLeonardo <40577375+GiovanniLeonardo@users.noreply.github.com>
2018-08-13 21:36:06 -0400

    Update app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,135 +1,122 @@
 // User clicks "Start" to start the game
 $('#start').on('click', function(){
-    game.start();
-    
-})
-
-// Provide user a button to stop the game when done.
-$('#done').on('click',function(){
-    game.done();
+  game.start();
+  
 })
 
 // After user clicks "Start" display the list (array) of questions and choice list
 var questions = [{
-    question: 'Question 0',
-    answers: ['a', 'b', 'c'],
-    correctAnswer: 'c'
+  question: 'Which is the only American Football team to go a whole season undefeated, including the Super Bowl?',
+  answers: [' The 1985 Chicago Bears',  ' The 2007 New England Patriots', ' The 1972 Miami Dolphins'],
+  correctAnswer: 'The 1972 Miami Dolphins',
+  name: 'Football',
+  
 },{
-    question: "Question 1",
-    answers: ["x", "y", "z"],
-    correctAnswer: "z"
+  question: "How many NBA championships did Michael Jordan win with the Chicago Bulls?",
+  answers: [" Five", " Six", " Seven"],
+  correctAnswer: "Six",
+  name: 'Basketball',
 },{
-    question: "Question 2",
-    answers: ["x", "y", "z"],
-    correctAnswer: "z"
+  question: "Which is the only team to play in every soccer World Cup tournament?",
+  answers: [" Brazil", " Spain", " Germany"],
+  correctAnswer: "Brazil",
+  name: 'Soccer',
 }, {
-    question: "Question 3",
-    answers: ["x", "y", "z"],
-    correctAnswer: "z"
+  question: "Who has won more tennis grand slam titles?",
+  answers: [" Rafael Nadal", " Serena Williams", " Roger Federer"],
+  correctAnswer: "Serena Williams",
+  name: 'Tennis',
 }, {
-    question: "Question 4",
-    answers: ["x", "y", "z"],
-    correctAnswer: "z"
+  question: "Which boxer inflicted Muhammad Ali's first defeat in professional boxing?",
+  answers: [" Larry Holmes", " Joe Frazier", " Leon Spinks"],
+  correctAnswer: "Joe Frazier",
+  name: 'Boxing',
 }, {
-    question: "Question 5",
-    answers: ["x", "y", "z"],
-    correctAnswer: "z"
+  question: "Which golf tournament did Tiger Woods win by 12 strokes in 1997 to record his first major championship win?",
+  answers: [" U.S. Open", " The PGA", " The Masters"],
+  correctAnswer: "The Masters",
+  name: 'Golf',
 }
 ];
 
+//
+  var correct = 0;
+  var incorrect = 0;
+  var counter = 30;
+  var unanswered = 0;
 
-var answers = {question:[0]="c", question:[1]="z", question:[2]="z" ,question:[3]="z" ,question:[4]="z", question:[5]="z"};
 
-// Set a counter and display the results, or a message if time ran out.
-    var game = {
-    correct: 0,
-    incorrect: 0,
-    counter: 30,
-    countdown: function(){  // start countdown when user clicks "Start"
-        game.counter--;
-        $('#counter').html(game.counter)
-        if(game.counter<=0){
-            console.log("Time ran out, sorry!"); // dsplay to the msg when game time is up
-            game.done();
-        }
-        //Check if user pressed "Done"
-        if($('#done').on('click', function(){
-        game.done();
-    }));
+// Set a counter and display the results, or a message if time runs out.
+  var game = {
+  
+  countdown: function(){  // start countdown when user clicks "Start"
+      counter--;
+
+      $('#counter').html(counter)
+      if(counter ==0){
+        //  console.log("Time ran out, sorry!"); // dsplay to the msg when game time is up
+          game.done();
+      }
+      //Check if user pressed "Done"
+      if($('#done').on('click', function(){
+      game.done();
+  }));
 },
-        // After use click "Start" remove the "Start" button and set the interval to xsec count down and display time remaining.
-        start: function(){
-        timer = setInterval(game.countdown,1000); 
-        $('#subcontainer').prepend('<h2>Time remaining: <span id="counter">30</span> Seconds</h2>'); // display time remaining
-        $('#start').remove(); 
-        // function for question loop
+      // After use click "Start" remove the "Start" button and set the interval to xsec count down and display time remaining.
+      start: function(){
+      timer = setInterval(game.countdown,1000); 
+      $('#subcontainer').prepend('<h2>Time remaining: <span id="counter">30</span> Seconds</h2>'); // display time remaining
+      $('#start').remove(); 
+      $('#intro').remove();
+      // function for question loop
+      for(var i=0; i<questions.length; i++){
+          $('#subcontainer').append('<h3>'+questions[i].question+'<h3>');
+          for(var j=0;j<questions[i].answers.length;j++){
+              //append each and every question with a name equal to the number of the question along with it's value.
+              $('#subcontainer').append("<input type='radio' name="+questions[i].name+" value='"+questions[i].answers[j]+"'>"+questions[i].answers[j]+"")
+          }
+      }
+      // Provide user with a "Done" button incase user finish early
+       $('#subcontainer').append('<br><br><button id="done">Done</button>')
+  },
+
+
+  //var answers = {question:[0]="c", question:[1]="z", question:[2]="z" ,question:[3]="z" ,question:[4]="z", question:[5]="z"};
+
+      // check whether answers are correct or incorrect
+      done: function(){
         for(var i=0; i<questions.length; i++){
-            $('#subcontainer').append('<h2>'+questions[i].question+'<h2>');
-            for(var j=0;j<questions[i].answers.length;j++){
-                //append each and every question with a name equal to the number of the question along with it's value.
-                $('#subcontainer').append("<input type='radio' name='question-"+i+" value='"+questions[i].answers[j]+"'>"+questions[i].answers[j]+"")
-            }
-        }
-        // Provide user with a "Done" button incase user finish early
-         $('#subcontainer').append('<br><button id="done">Done</button>')
-    },
-    
-        // check whether answers are correct or incorrect
-        done: function(){
-            $.each($('input[name="question-0"]:checked'), function(){  // looks for the checked / selected answer
-                if($(this).val() === questions[0].correctAnswer){
-                    game.correct++;
-                } else {
-                    game.incorrect++;
-                }
-            }),
-            $.each($('input[name="question-1"]:checked'), function(){  // looks for the checked / selected answer
-                if($(this).val() === questions[1].correctAnswer){
-                    game.correct++;
-                } else {
-                    game.incorrect++;
-                }
-            }),
-            $.each($('input[name="question-2"]:checked'), function(){  // looks for the checked / selected answer
-                if($(this).val() === questions[2].correctAnswer){
-                    game.correct++;
-                } else {
-                    game.incorrect++;
-                }
-            }),
-            $.each($('input[name="question-3"]:checked'), function(){  // looks for the checked / selected answer
-                if($(this).val() === questions[3].correctAnswer){
-                    game.correct++;
-                } else {
-                    game.incorrect++;
-                }
-            }),
-            $.each($('input[name="question-4"]:checked'), function(){  // looks for the checked / selected answer
-                if($(this).val() === questions[4].correctAnswer){
-                    game.correct++;
-                } else {
-                    game.incorrect++;
-                }
-            }),
-            $.each($('input[name="question-5"]:checked'), function(){  // looks for the checked / selected answer
-                if($(this).val() === questions[5].correctAnswer){
-                    game.correct++;
-                } else {
-                    game.incorrect++;
-                } 
-            }), 
+        var userChoice = $('input[type="radio"][name='+questions[i].name+']:checked').val();
+        
+        if(userChoice === questions[i].correctAnswer){
+          correct++;
+      } else if (!userChoice){
+          unanswered++
+      }
+        else{
+            incorrect++;
+      }
+    };
 
-            this.result();
-            },
+          this.result();
+          },
 
-            // show the results
-            result: function(){
-                clearInterval(timer);
-                $('#subcontainer h2').remove();
-                $('#subcontainer').html("<h2>Done!!</h2>");
-                $('#subcontainer').append("<h3>Correct Answer: "+this.correct);
-                $('#subcontainer').append("<h3>Incorrect Answer: "+this.incorrect);
-                $('#subcontainer').append("<h3>Unanswered: "+(questions.length-(this.incorrect+this.correct))+"</h3>");
-            },
-        }
-    
+          // show the results
+          result: function(){
+            var calculation = questions.length-(correct+incorrect);
+              clearInterval(timer);
+              $('#subcontainer h2').remove();
+              $('#subcontainer').html("<h2>Done!!</h2>");
+              $('#subcontainer').append("<h3>Correct Answer: "+correct);
+              $('#subcontainer').append("<h3>Incorrect Answer: "+incorrect);
+              $('#subcontainer').append("<h3>Unanswered: "+calculation+"</h3>");
+              
+          },
+      }
+      // Provide user a button to stop the game when done, 
+      $(document).ready(function(){
+
+        $('#subcontainer').on('click', '#done', function(){
+          game.done();
+        })
+        });
